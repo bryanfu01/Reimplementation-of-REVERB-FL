5c8aef60d48ddeb4d93d834474844bae23c97242
Bryan Fu <bryanfu@Bryans-MacBook-Air.local>
2026-03-05 18:45:58 -0800

    Continuing pgd with uniform distribution attack

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
     num_clients = 10
     simulation = Simulator(num_clients=num_clients, is_iid=True)
     num_rounds = 60
-    """
+    
     simulation.run_simulation(
-        checkpoint_path="/content/drive/MyDrive/REVERB-FL_PGD_iid_checkpoint_round_45.pt"
+        checkpoint_path="/content/drive/MyDrive/REVERB-FL_PGD_iid_checkpoint_round_20.pt"
     )
     """
     # simulation.run_simulation(attack_type = None, framework_active=False, num_rounds=num_rounds)
@@ -22,6 +22,7 @@ def main():
     simulation.run_simulation(attack_type = 'pgd', framework_active=True, num_rounds=num_rounds)
     
     # simulation.run_simulation(attack_type = 'awgn', framework_active=True, num_rounds=num_rounds)
+    """
 
 if __name__ == "__main__":
     torch.multiprocessing.set_start_method('spawn', force=True)
