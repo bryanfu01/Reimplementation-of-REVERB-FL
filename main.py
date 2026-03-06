04abcc4c5a50e0b932afbf304ce0bbbd694856f3
Bryan Fu <bryanfu@Bryans-MacBook-Air.local>
2026-03-06 14:36:43 -0800

    Continuing PGD non-iid

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ def main():
     num_rounds = 60
     
     simulation.run_simulation(
-        checkpoint_path="/content/drive/MyDrive/Baseline_FedAvg_AWGN_non-iid_checkpoint_round_60.pt"
+        checkpoint_path="/content/drive/MyDrive/REVERB-FL_PGD_non-iid_checkpoint_round_10.pt"
     )
     """
     simulation.run_simulation(attack_type = None, framework_active=False, num_rounds=num_rounds)
     simulation.run_simulation(attack_type = 'fgsm', framework_active=False, num_rounds=num_rounds)
     simulation.run_simulation(attack_type = 'pgd', framework_active=False, num_rounds=num_rounds)
     simulation.run_simulation(attack_type = 'awgn', framework_active=False, num_rounds=num_rounds)
-    """
+    
 
     print("Starting REVERB-FL framework ...")
 
     simulation.run_simulation(attack_type = None, framework_active=True, num_rounds=num_rounds)
     simulation.run_simulation(attack_type = 'fgsm', framework_active=True, num_rounds=num_rounds)
-    simulation.run_simulation(attack_type = 'pgd', framework_active=True, num_rounds=num_rounds)  
+    simulation.run_simulation(attack_type = 'pgd', framework_active=True, num_rounds=num_rounds)
+    """  
     simulation.run_simulation(attack_type = 'awgn', framework_active=True, num_rounds=num_rounds)
 
 if __name__ == "__main__":
